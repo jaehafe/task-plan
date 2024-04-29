b8a6296be6c6400ef5e39bc298913c85c3146105
jaehafe <wogkdkrm112@gmail.com>
2024-04-29 19:26:02 +0900

    feat: csv 읽어와서 print

--- src/main/java/com/example/taskplan/TaskPlanApplication.java
+++ src/main/java/com/example/taskplan/TaskPlanApplication.java
@@ -1,9 +1,11 @@
 package com.example.taskplan;
 
 import com.example.taskplan.event.*;
+import com.example.taskplan.reader.EventCsvReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -12,42 +14,16 @@ import java.util.stream.Collectors;
 
 public class TaskPlanApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        List<AbstractEvent> list = new ArrayList<>();
         Schedule schedule = new Schedule();
-        HashSet<String> participants = new HashSet<>();
-        participants.add("jaeha");
 
-        Meeting meeting1 = new Meeting(
-                1,
-                "meeting1",
-                ZonedDateTime.now(),
-                ZonedDateTime.now().plusHours(1),
-                participants,
-                "meetingRoomA",
-                "스터디"
-        );
-        schedule.add(meeting1);
+        EventCsvReader csvReader = new EventCsvReader();
+        String meetingCsvPath = "/data/meeting.csv";
 
-        Todo todo1 = new Todo(
-                2,
-                "todo1",
-                ZonedDateTime.now().plusHours(3),
-                ZonedDateTime.now().plusHours(4),
-                "할 일"
-        );
-        Todo todo2 = new Todo(
-                3,
-                "todo2",
-                ZonedDateTime.now().plusHours(4),
-                ZonedDateTime.now().plusHours(5),
-                "할 일"
-        );
-        schedule.add(todo1);
+        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+        meetings.forEach(schedule::add);
 
         schedule.printAll();
-
-        schedule.printBy(EventType.TO_DO);
-
     }
 }
